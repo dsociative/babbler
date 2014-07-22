7f09ee133e8e8ee1c3a3c670a4b4784851cce15a
dsociative <admin@geektech.ru>
2014-07-22 23:08:39 +0400

    MessageWriter.pack works

--- src/main/java/MessageWriter.java
+++ src/main/java/MessageWriter.java
@@ -1,7 +1,13 @@
 import org.json.JSONObject;
 
+import java.nio.ByteBuffer;
+
 public class MessageWriter {
     public byte[] pack(JSONObject message) {
-        return new byte[0];
+        byte[] data = message.toString().getBytes();
+        ByteBuffer size = ByteBuffer.allocate(4 + data.length);
+        size.putInt(data.length);
+        size.put(data);
+        return size.array();
     }
 }
